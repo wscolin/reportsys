a14bf54de143879f8c1d4fe36d2859fdcd7d57d3
colin <mrcolin@126.com>
2020-09-16 18:13:34 +0800

    动态出图1.6

--- src/main/java/com/zxd/report/controller/PlotController.java
+++ src/main/java/com/zxd/report/controller/PlotController.java
@@ -40,12 +40,98 @@ public class PlotController {
     @ResponseBody
     public String canvos(HttpServletRequest request)throws Exception{
         String  kmbm =   request.getParameter("kmbm");
+        String index  = request.getParameter("index");
         String table = kmbm.equals("A024")?"t_disbursebyarea":"t_incomebyarea ";
-        Map returnMap =  new HashMap();
         //get当前年份
         int year = DateUtil.getYear();
         //get昨年
         int last_year = year -1;
+        Map returnMap =  new HashMap();
+        if(index.equals("11")){
+            getLjdata("A001", table, returnMap, year, last_year);
+            getDydataFor11("A027", table, returnMap, year, last_year);
+        }else{
+            getLjdata(kmbm, table, returnMap, year, last_year);
+            getDydata(kmbm, table, returnMap, year, last_year);
+        }
+
+
+
+        String returnData = gson.toJson(returnMap);
+
+        return returnData;
+    }
+
+    private void getDydata(String kmbm, String table, Map returnMap, int year, int last_year) {
+        //当年本月
+        String this_year_dy_sql = stParamsService.getParamConf("plot_dy").replaceAll("\\$\\{year\\}",year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
+        List<Map> this_year_dy_list = stAutoMapper.selectBysqlList(this_year_dy_sql);
+
+        String last_year_dy_sql = stParamsService.getParamConf("plot_lj").replaceAll("\\$\\{year\\}",last_year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
+        //去年本月
+        List<Map> last_year_dy_list = stAutoMapper.selectBysqlList(last_year_dy_sql);
+
+        //当年当月同比百分比
+        String this_year_dy_tb_sql = stParamsService.getParamConf("plot_dy_tb_bfb").replaceAll("\\$\\{this_year\\}",year+"").
+                replaceAll("\\$\\{last_year\\}",last_year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
+        List<Map> this_year_dy_tb_list = stAutoMapper.selectBysqlList(this_year_dy_tb_sql);
+
+        //去年当月同比百分比
+        String last_year_dy_tb_sql = stParamsService.getParamConf("plot_dy_tb_bfb").replaceAll("\\$\\{this_year\\}",last_year+"").
+                replaceAll("\\$\\{last_year\\}",last_year-1+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
+        List<Map> last_year_dy_tb_list = stAutoMapper.selectBysqlList(last_year_dy_tb_sql);
+
+        List this_year_dy_Array = new ArrayList();
+        this.map2list(this_year_dy_list.get(0),this_year_dy_Array);
+        List last_year_dy_Array = new ArrayList();
+        this.map2list(last_year_dy_list.get(0),last_year_dy_Array);
+        List this_year_dy_tb_Array = new ArrayList();
+        this.map2list(this_year_dy_tb_list.get(0),this_year_dy_tb_Array);
+        List last_year_dy_tb_Array = new ArrayList();
+
+
+        this.map2list(last_year_dy_tb_list.get(0),last_year_dy_tb_Array);
+        returnMap.put("this_year_dy_Array",this_year_dy_Array);
+        returnMap.put("last_year_dy_Array",last_year_dy_Array);
+        returnMap.put("this_year_dy_tb_Array",this_year_dy_tb_Array);
+        returnMap.put("last_year_dy_tb_Array",last_year_dy_tb_Array);
+    }
+    private void getDydataFor11(String kmbm, String table, Map returnMap, int year, int last_year) {
+        //当年本月
+        String this_year_dy_sql = stParamsService.getParamConf("plot_lj").replaceAll("\\$\\{year\\}",year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
+        List<Map> this_year_dy_list = stAutoMapper.selectBysqlList(this_year_dy_sql);
+
+        String last_year_dy_sql = stParamsService.getParamConf("plot_lj").replaceAll("\\$\\{year\\}",last_year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
+        //去年本月
+        List<Map> last_year_dy_list = stAutoMapper.selectBysqlList(last_year_dy_sql);
+
+        //当年当月同比百分比
+        String this_year_dy_tb_sql = stParamsService.getParamConf("plot_bfb").replaceAll("\\$\\{this_year\\}",year+"").
+                replaceAll("\\$\\{last_year\\}",last_year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
+        List<Map> this_year_dy_tb_list = stAutoMapper.selectBysqlList(this_year_dy_tb_sql);
+
+        //去年当月同比百分比
+        String last_year_dy_tb_sql = stParamsService.getParamConf("plot_bfb").replaceAll("\\$\\{this_year\\}",last_year+"").
+                replaceAll("\\$\\{last_year\\}",last_year-1+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
+        List<Map> last_year_dy_tb_list = stAutoMapper.selectBysqlList(last_year_dy_tb_sql);
+
+        List this_year_dy_Array = new ArrayList();
+        this.map2list(this_year_dy_list.get(0),this_year_dy_Array);
+        List last_year_dy_Array = new ArrayList();
+        this.map2list(last_year_dy_list.get(0),last_year_dy_Array);
+        List this_year_dy_tb_Array = new ArrayList();
+        this.map2list(this_year_dy_tb_list.get(0),this_year_dy_tb_Array);
+        List last_year_dy_tb_Array = new ArrayList();
+
+
+        this.map2list(last_year_dy_tb_list.get(0),last_year_dy_tb_Array);
+        returnMap.put("this_year_dy_Array",this_year_dy_Array);
+        returnMap.put("last_year_dy_Array",last_year_dy_Array);
+        returnMap.put("this_year_dy_tb_Array",this_year_dy_tb_Array);
+        returnMap.put("last_year_dy_tb_Array",last_year_dy_tb_Array);
+    }
+
+    private void getLjdata(String kmbm, String table, Map returnMap, int year, int last_year) {
         String lj_sql = stParamsService.getParamConf("plot_lj").replaceAll("\\$\\{year\\}",year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
         //当年累计
         List<Map> this_year_list = stAutoMapper.selectBysqlList(lj_sql);
@@ -75,46 +161,6 @@ public class PlotController {
         returnMap.put("this_year_tb_Array",this_year_tb_Array);
         returnMap.put("last_year_tb_Array",last_year_tb_Array);
         returnMap.put("result","success");
-
-
-
-        //当年本月
-        String this_year_dy_sql = stParamsService.getParamConf("plot_dy").replaceAll("\\$\\{year\\}",year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
-        List<Map> this_year_dy_list = stAutoMapper.selectBysqlList(this_year_dy_sql);
-
-        String last_year_dy_sql = stParamsService.getParamConf("plot_lj").replaceAll("\\$\\{year\\}",last_year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
-        //去年本月
-        List<Map> last_year_dy_list = stAutoMapper.selectBysqlList(last_year_dy_sql);
-
-        //当年当月同比百分比
-        String this_year_dy_tb_sql = stParamsService.getParamConf("plot_dy_tb_bfb").replaceAll("\\$\\{this_year\\}",year+"").
-                replaceAll("\\$\\{last_year\\}",last_year+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
-        List<Map> this_year_dy_tb_list = stAutoMapper.selectBysqlList(this_year_dy_tb_sql);
-
-        //去年当月同比百分比
-        String last_year_dy_tb_sql = stParamsService.getParamConf("plot_dy_tb_bfb").replaceAll("\\$\\{this_year\\}",last_year+"").
-                replaceAll("\\$\\{last_year\\}",last_year-1+"").replaceAll("\\$\\{kmbm\\}",kmbm+"").replaceAll("\\$\\{table\\}",table+"");
-        List<Map> last_year_dy_tb_list = stAutoMapper.selectBysqlList(last_year_dy_tb_sql);
-
-        List this_year_dy_Array = new ArrayList();
-        this.map2list(this_year_dy_list.get(0),this_year_dy_Array);
-        List last_year_dy_Array = new ArrayList();
-        this.map2list(last_year_dy_list.get(0),last_year_dy_Array);
-        List this_year_dy_tb_Array = new ArrayList();
-        this.map2list(this_year_dy_tb_list.get(0),this_year_dy_tb_Array);
-        List last_year_dy_tb_Array = new ArrayList();
-
-
-
-        this.map2list(last_year_dy_tb_list.get(0),last_year_dy_tb_Array);
-        returnMap.put("this_year_dy_Array",this_year_dy_Array);
-        returnMap.put("last_year_dy_Array",last_year_dy_Array);
-        returnMap.put("this_year_dy_tb_Array",this_year_dy_tb_Array);
-        returnMap.put("last_year_dy_tb_Array",last_year_dy_tb_Array);
-
-        String returnData = gson.toJson(returnMap);
-
-        return returnData;
     }
 
 
